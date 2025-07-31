069b21be231fd8c03f3ae9de0f01cef45b8c5abb
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-07-31 21:46:50 +0530

    Case study day 9 testing

---
+++ EmailValidationApp/Services/RegistrationService.cs
@@ -0,0 +1,29 @@
+﻿using EmailValidationApp.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailValidationApp.Services
+{
+    public class RegistrationService
+    {
+        private readonly EmailValidator _validator;
+
+        public RegistrationService(EmailValidator validator)
+        {
+            _validator = validator;
+        }
+
+        public string Register(string email)
+        {
+            if (_validator.IsEmailValid(email))
+            {
+                return "Registration successful.";
+            }
+            return "Invalid email address.";
+        }
+    }
+
+}
---
+++ EmailValidationApp/Validators/EmailValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailValidationApp.Validators
+{
+    public class EmailValidator
+    {
+        public bool IsEmailValid(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email)) return false;
+
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            return Regex.IsMatch(email, pattern, RegexOptions.IgnoreCase);
+        }
+    }
+
+}
