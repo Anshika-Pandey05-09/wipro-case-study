4ce0abcf39124e9999573a0df8aba21917b4a593
Anshika-Pandey05-09 <pandeyanshika353@gmail.com>
2025-07-31 21:50:26 +0530

    Day 9 Testing MSTests

---
+++ EmailValidationApp.Tests/Test1.cs
@@ -0,0 +1,25 @@
+﻿using EmailValidationApp.Validators;
+
+namespace EmailValidationApp.Tests
+{
+    [TestClass]
+    public class EmailValidatorMSTest
+    {
+        private readonly EmailValidator _validator = new();
+
+        [TestMethod]
+        public void ValidEmail_ShouldReturnTrue()
+        {
+            Assert.IsTrue(_validator.IsEmailValid("test@example.com"));
+        }
+
+        [TestMethod]
+        public void InvalidEmail_ShouldReturnFalse()
+        {
+            Assert.IsFalse(_validator.IsEmailValid("abc@"));
+            Assert.IsFalse(_validator.IsEmailValid("abc.com"));
+            Assert.IsFalse(_validator.IsEmailValid("@xyz.com"));
+        }
+    }
+
+}
